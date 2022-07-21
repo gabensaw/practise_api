e9c976dc09edae010a5524314eab3339aaf198c0
gabensaw <gabriel.sawicki1995@gmail.com>
2022-07-21 17:20:23 +0100

    date_time_list method created - contain 5 day forecast timestamps

--- service.py
+++ service.py
@@ -1,4 +1,5 @@
 from single_city import City
 
 city = City('London')
-print(city.response_data().http_code)
+
+print(city.response_data().date_time_list())
--- single_city_model.py
+++ single_city_model.py
@@ -2,3 +2,10 @@ class SingleCityModel:
 
     def __init__(self, city):
         self.http_code = city['cod']
+        self.forecasts = city['list']
+
+    def date_time_list(self):
+        date_time_list = []
+        for timestamp in self.forecasts:
+            date_time_list.append(timestamp['dt_txt'])
+        return date_time_list

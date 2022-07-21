2b41a865dd65e674c1b3980e70f81f7466baf700
gabensaw <gabriel.sawicki1995@gmail.com>
2022-07-21 17:54:33 +0100

    added upcoming forecast message (temperature, pressure, humidity) for selected city by user

--- service.py
+++ service.py
@@ -1,5 +1,18 @@
 from single_city import City
 
-city = City('London')
+city_name = input('Provide city to check upcoming weather forecast: ')
 
-print(city.response_data().date_time_list())
+city = City(city_name)
+
+message = f'{city_name}, temperature {city.response_data().upcoming_temperature},' \
+          f' pressure {city.response_data().upcoming_pressure}, humidity {city.response_data().upcoming_humidity} on ' \
+          f'{city.response_data().date_time_list()[0]}'
+
+print(message)
+
+# print(city.response_data().date_time_list())
+# print(city.response_data().upcoming_forecast)
+#
+# print(city.response_data().upcoming_temperature)
+# print(city.response_data().upcoming_pressure)
+# print(city.response_data().upcoming_humidity)
--- single_city_model.py
+++ single_city_model.py
@@ -3,6 +3,10 @@ class SingleCityModel:
     def __init__(self, city):
         self.http_code = city['cod']
         self.forecasts = city['list']
+        self.upcoming_forecast = city['list'][0]
+        self.upcoming_temperature = '{:.1f} °C'.format(round(self.upcoming_forecast['main']['temp'] - 273.15, 1))
+        self.upcoming_pressure = f"{self.upcoming_forecast['main']['pressure']} hPa"
+        self.upcoming_humidity = f"{self.upcoming_forecast['main']['humidity']} %"
 
     def date_time_list(self):
         date_time_list = []
